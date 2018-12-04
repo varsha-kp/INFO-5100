ae3e1d314d2df5775ceded2a160483e754c3d789
Varsha Premani <premani.v@husky.neu.edu>
2018-12-04 13:05:32 -0500

    Input Enterprise Admin role and JPanel added

---
+++ TrafficCongestionProject/src/business/Role/InputEnterpriseAdminRole.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package business.Role;
+
+import business.EcoSystem;
+import business.Enterprise.Enterprise;
+import business.Organization.ConstructionManagerOrganization;
+import business.Organization.Organization;
+import business.UserAccount.UserAccount;
+import javax.swing.JPanel;
+import business.Network.Network;
+import userInterface.AdministrativeRole.InputEnterpriseAdminJPanel;
+
+/**
+ *
+ * @author VARSHA PREMANI
+ */
+public class InputEnterpriseAdminRole extends Role {
+    
+    public static Organization.OrganizationType[] types = {Organization.OrganizationType.Commuter,Organization.OrganizationType.SystemEngineer};
+     @Override
+     public JPanel createWorkArea(JPanel userProcessContainer, UserAccount account, Organization organization, Enterprise enterprise, EcoSystem business) {
+        return new InputEnterpriseAdminJPanel(userProcessContainer, enterprise,business); }
+    
+}
