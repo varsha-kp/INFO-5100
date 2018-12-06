148e884b6e9bb97de2b8773bedbbb20b755a779d
BarkhaSamyani <BarkhaSamyani06@gmail.com>
2018-12-06 00:49:34 -0500

    Adding construction employee and SystemEngineer  Role

---
+++ TrafficCongestionProject/src/business/Role/ConstructionEmployee.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package business.Role;
+
+import business.EcoSystem;
+import business.Enterprise.Enterprise;
+import business.Organization.ConstructionEmployeeOrganization;
+import business.Organization.ConstructionManagerOrganization;
+import business.Organization.Organization;
+import business.UserAccount.UserAccount;
+import javax.swing.JPanel;
+import userInterface.ConstructionEmployeeRole.ConstructionEmployeeWorkAreaJPanel;
+
+/**
+ *
+ * @author barkhasamyani
+ */
+public class ConstructionEmployee extends Role{
+      @Override
+    public JPanel createWorkArea(JPanel userProcessContainer, UserAccount account, Organization organization, Enterprise enterprise, EcoSystem business) {
+        return new ConstructionEmployeeWorkAreaJPanel(userProcessContainer, account, (ConstructionEmployeeOrganization)organization, enterprise);
+    }
+    
+}
